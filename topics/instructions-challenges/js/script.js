217dfdb21859701aff53f87d89597ae9d6aad292
CFutur <clara.fioramore@gmail.com>
2026-09-15 15:53:40 -0400

    tried drawing the first mountain with a light ish green

--- topics/instructions-challenges/js/script.js
+++ topics/instructions-challenges/js/script.js
@@ -23,5 +23,56 @@ function setup() {
 function draw() {
     // Orange background for sunset
     background(255, 172, 28)
+    // No stroke on our beautiful landscape
+    noStroke()
+
+    drawPrairie()
+
+}
+
+
+/*
+*Draws mountains and flowers using functions
+* */
+
+function DrawPrairie() {
+    drawMountains();
+    drawSunset();
+    drawFlowers();
+}
+
+/**
+ * Draw the mountains with different colours of green for depth
+ */
+
+function drawMountains() {
+    push();
+    fill(84, 140, 84);
+    ellipse(200, 300, 400);
+    pop();
+
+
+
+
+}
+
+/**
+ * Draw the sunset with different colours of orange for depth
+ */
+
+function drawSunset() {
+
+
+
+}
+
+/**
+ * Draw the flowers with variety
+ */
+
+function drawFlowers() {
+
+
+
 
 }
