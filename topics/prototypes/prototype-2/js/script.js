b8dadb0726d012b8d7f8d143d25d8bc27fe5cf4f
CFutur <clara.fioramore@gmail.com>
2026-09-21 20:10:53 -0400

    described my functions before starting to set up my draw function and the image on the cursor

--- topics/prototypes/prototype-2/js/script.js
+++ topics/prototypes/prototype-2/js/script.js
@@ -9,16 +9,40 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * This serves to create a canvas!!
 */
 function setup() {
+    createcanvas(1000, 600);
+
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * my draw will be adding a puppy from the neck up
 */
 function draw() {
+    //blue background for cute puppy
+    background(196, 231, 247)
+    //no strokes allowed again cause we love smooth
+    noStroke();
+
+    drawDog();
+
+     // Draw a long thin circle as a collar for the cursor
+    push();
+    // No line around the shape
+    noStroke();
+    // Make it red
+    fill(210, 43, 43);
+    // Draw a 110x10
+    ellipse(mouseX, mouseY, 110, 10);
+    //make another planet on x axis dark blue
+    fill(28,37,60)
+    ellipse(mouseX, mouseX, 150, 150)
+    //make a last planet on y axis red
+    fill(60,28,49)
+    ellipse(mouseY, mouseY, 80, 80)
+    pop();
 
 }
