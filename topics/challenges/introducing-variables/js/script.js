67efc225f9c796369db198a1e17d4945864d2e38
CFutur <clara.fioramore@gmail.com>
2026-09-22 11:17:52 -0400

    begin the introducing variables challenge

---
+++ topics/challenges/introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing variables
+ * Clara Fioramore
+ * 
+ * This is simply a project to learn how to use variables and what it does
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
