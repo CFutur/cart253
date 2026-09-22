e6644b15e5ff894e4e577671f99f0ebe76e54307
CFutur <clara.fioramore@gmail.com>
2026-09-21 21:42:07 -0400

    changed read me heading and added one white eyeball and the dogs face

--- topics/prototypes/prototype-2/js/script.js
+++ topics/prototypes/prototype-2/js/script.js
@@ -12,7 +12,7 @@
  * This serves to create a canvas!!
 */
 function setup() {
-    createcanvas(1000, 600);
+    createCanvas(1000, 600);
 
 
 }
@@ -29,14 +29,14 @@ function draw() {
 
     drawDog();
 
-     // Draw a long thin circle as a collar for the cursor
+    // Draw a small green planet at mouse position
     push();
     // No line around the shape
     noStroke();
-    // Make it red
-    fill(210, 43, 43);
-    // Draw a 110x10
-    ellipse(mouseX, mouseY, 110, 10);
+    // Make it green
+    fill(20,80,81);
+    // Draw a 110x110 planet mouse position
+    ellipse(mouseX, mouseY, 110, 110);
     //make another planet on x axis dark blue
     fill(28,37,60)
     ellipse(mouseX, mouseX, 150, 150)
@@ -44,5 +44,53 @@ function draw() {
     fill(60,28,49)
     ellipse(mouseY, mouseY, 80, 80)
     pop();
+
+     //* Draw a long thin circle as a collar for the cursor
+   // push();
+    // No line around the shape
+    //noStroke();
+    // Make it red
+   // fill(210, 43, 43);
+    // Draw a 110x10
+   // ellipse(mouseX, mouseY, 110, 110);
+    //add a small golden circle acting as a bell in the middle and a bit lowered
+   // fill(255, 224, 46)
+    //ellipse(mouseX, mouseY, 150, 150)
+   // pop();
+
+}
+
+//Draw a dogs face, eyes, ears and snoot using functions
+
+function drawDog(){
+    drawEye();
+    drawSnoot();
+    drawFace();
+    drawEars();
+
+}
+
+//Drawing the doggies eyes
+
+//Starting with the two white eyeballs
+function drawEye(){
+    push();
+    //Left white eyeball
+    fill(255);
+    ellipse(300, 300, 100, 100)
+    pop();
+
+
+}
+
+//Just drawing the simple circle for the dog's face
+
+function drawFace(){
+
+push();
+//fill with golden retriever golden colour
+fill(215, 148, 46);
+ellipse(500, 300, 250, 250);
+pop();
 
 }
